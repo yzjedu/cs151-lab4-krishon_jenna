cdcaad290f3abc21d3ad42fc3fc97268ef0842e9
Pkrishon <Kpinkins@loyola.edu>
2024-10-03 14:03:06 -0400

    Began code

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-# Read the README.md
-# Read it again
-# Your code here
-# Delete these 4 lines of comments
+# Programmed by Jenna and Krishon Pinkins
+# Loyola CS151, Professor Zee
+# Due Date: October 3, 2024
+# Lab Assignment: 04
+
+
+
+
+sub_package = input('What is your cellular package?')
+sub_package = sub_package.lower()
+
+while sub_package !='Green' or sub_package !='Blue' or sub_package !='Purple':
+    print('Input Invalid! Please enter a valid cellular package')
+    sub_package = input('What is your cellular package?' )
+    sub_package = sub_package.lower()
+
+used_gigs = input('How many gigabytes have you used?:')
