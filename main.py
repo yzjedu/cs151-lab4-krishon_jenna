da6478685bf86dc7ddb2c45a6af285bc79d4ceef
Pkrishon <Kpinkins@loyola.edu>
2024-10-03 20:24:11 -0400

    Began code

--- main.py
+++ main.py
@@ -9,9 +9,23 @@
 sub_package = input('What is your cellular package?')
 sub_package = sub_package.lower()
 
-while sub_package !='Green' or sub_package !='Blue' or sub_package !='Purple':
-    print('Input Invalid! Please enter a valid cellular package')
-    sub_package = input('What is your cellular package?' )
-    sub_package = sub_package.lower()
+while True:
+    if sub_package == 'Green' or sub_package == 'Blue' or sub_package == 'Purple':
+    else:
+        print('Input Invalid! Please enter a valid cellular package.')
+        sub_package = input('What is your cellular package?')
+        sub_package = sub_package.lower()
+        break
 
-used_gigs = input('How many gigabytes have you used?:')
+used_gigs = int(input('How many gigabytes have you used?:'))
+
+if sub_package == 'Green':
+    coupon = input('Do you have a coupon?')
+    if coupon == 'Yes':
+        cellular_cost = ((49.99 + 15 * (used_gigs - 2) - 20))
+    else: cellular_cost = ((49.99 + 15 * (used_gigs - 2)))
+
+elif sub_package == 'Blue':
+    cellular_cost = (70 + 10 * (used_gigs - 4))
+
+
